5ac1f4b95bb2963bc46d9eb3aec55bc094a920d0
Jiachen Wang <Jiachen775@gmail.com>
2017-02-04 16:22:24 -0500

    3rd page

--- app/src/main/java/com/example/richard/prescript/MessageSubmit.java
+++ app/src/main/java/com/example/richard/prescript/MessageSubmit.java
@@ -12,7 +12,7 @@ public class MessageSubmit extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_verification);
+        setContentView(R.layout.activity_camera_display);
     }
 
     public void sendMessage (View view) {
