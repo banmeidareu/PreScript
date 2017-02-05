5b513136a2ed3ef85fc5ce8d193244393d7146ba
RLKANG <richrichard2002@yahoo.ca>
2017-02-05 01:17:31 -0500

    Debugging 1:17am

--- app/src/main/java/com/example/richard/prescript/CameraVerify.java
+++ app/src/main/java/com/example/richard/prescript/CameraVerify.java
@@ -124,6 +124,11 @@ public class CameraVerify extends AppCompatActivity {
         takePictureButton.setText("Take Picture");
     }
 
+    public void GoHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
     private void copyFiles() {
         try {
             //location we want the file to be at
