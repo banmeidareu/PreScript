e3cff9e8cb3a8347220788077ec2b6dd12363e50
Jiachen Wang <Jiachen775@gmail.com>
2017-02-04 22:13:38 -0500

    display

--- app/src/main/java/com/example/richard/prescript/CameraDisplay.java
+++ app/src/main/java/com/example/richard/prescript/CameraDisplay.java
@@ -8,15 +8,88 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.File;
 import java.io.IOException;
 
 
 public class CameraDisplay extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState){
+    String s;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera_display);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            s = extras.getString("perscription");
+        }
+
     }
+
+    protected String parseDose(String s) {
+        String[] words = s.split("\\s+");    // splits s into it's words
+        String dosage = "0"; //default dosage
+        for (int i = 0; i < s.length(); i++) {
+            if (words[i] == "Take" || words[i] == "take") { // if matches TAKE
+
+                dosage = words[i] + " " + words[i + 1];
+            }
+        }
+        return dosage;
+    }
+
+    protected String parseTimes(String s) {
+        String[] words = s.split("\\s+");    // splits s into it's words
+        String times = "0"; //default dosage
+        for (int i = 0; i < s.length(); i++) {
+            if (words[i] == "times" || words[i] == "time") { // if matches TAKE
+
+                times = words[i - 1] + " " + words[i] + " " + words[i + 1];
+            }
+        }
+        return times;
+    }
+
+    protected String parseDuration(String s) {
+        String[] words = s.split("\\s+");    // splits s into it's words
+        String duration = "0"; //default dosage
+        for (int i = 0; i < s.length(); i++) {
+            if (words[i] == "days" || words[i] == "day") { // if matches TAKE
+
+                duration= words[i - 1] + " " + words[i];
+            }
+        }
+        return duration;
+    }
+
+    protected String parseName(String s){
+        String[] words = s.split("\\s+");    // splits s into it's words
+        String name = "0"; //default dosage
+        for (int i = 0; i < s.length(); i++) {
+            if (words[i] == "MG") { // if matches TAKE
+
+                name = words[i - 2] + " " + words[i-1] +" "+ words[i];
+            }
+        }
+        return name;
+    }
+
+    protected String fullString (String s){
+        String dose = parseDose(s);
+        String times = parseTimes(s);
+        String duration = parseDuration(s);
+        String name = parseName(s);
+
+        String full = dose + " " + name + " " + times + " " + duration + "\n";
+
+        return full;
+
+    }
+
+    public File TempFile(Context context, String url) {
+        File file = new File(context.getFilesDir(),filename )
 
 
 }
+
